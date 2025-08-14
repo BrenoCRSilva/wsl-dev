cea2f52551288f8910358d33efebcfce71039141
Breno Rodrigues <brenocrs@gmail.com>
2025-08-14 13:06:11 -0300

    fix zsh

--- runs/zsh.sh
+++ runs/zsh.sh
@@ -2,4 +2,10 @@
 
 sudo pacman -S zsh
 hash -r
-chsh -s $(which zsh)
+
+# Add zsh to /etc/shells and change shell
+ZSH_PATH=$(which zsh)
+if ! grep -q "^$ZSH_PATH$" /etc/shells; then
+    echo "$ZSH_PATH" | sudo tee -a /etc/shells
+fi
+chsh -s "$ZSH_PATH"
