dc034b65e5c5803b6fd09b62ffbc455bdeb059a7
Breno Rodrigues <brenocrs@gmail.com>
2025-08-14 13:43:37 -0300

    shell script improvement

--- runs/zsh.sh
+++ runs/zsh.sh
@@ -1,11 +1,16 @@
+
 #!/usr/bin/env bash
 
-sudo pacman -S zsh
-hash -r
+sudo pacman -S --noconfirm zsh
+hash -r  # refresh shell hash
 
-# Add zsh to /etc/shells and change shell
+CURRENT_USER=${USER:-$(whoami)}
+
 ZSH_PATH=$(which zsh)
+
 if ! grep -q "^$ZSH_PATH$" /etc/shells; then
     echo "$ZSH_PATH" | sudo tee -a /etc/shells
 fi
-chsh -s "$ZSH_PATH"
+
+chsh -s "$ZSH_PATH" "$CURRENT_USER"
+
